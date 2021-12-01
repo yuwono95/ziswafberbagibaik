d1f4a1048abfd8d52e848e12a567d2aad6a759cf
Yuwono Mujahidin <yuwono@badr-interactive.com>
2021-12-01 22:27:35 +0700

    Update edit.blade.php

--- resources/views/admin/inputPerolehans/edit.blade.php
+++ resources/views/admin/inputPerolehans/edit.blade.php
@@ -55,6 +55,7 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.inputPerolehan.fields.buktitransfer_helper') }}</span>
             </div>
+            @if(auth()->user()->team_admin)
             <div class="form-group">
                 <label for="verifiedstatus_id">{{ trans('cruds.inputPerolehan.fields.verifiedstatus') }}</label>
                 <select class="form-control select2 {{ $errors->has('verifiedstatus') ? 'is-invalid' : '' }}" name="verifiedstatus_id" id="verifiedstatus_id">
@@ -69,6 +70,7 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.inputPerolehan.fields.verifiedstatus_helper') }}</span>
             </div>
+            @endif
             <div class="form-group">
                 <button class="btn btn-danger" type="submit">
                     {{ trans('global.save') }}
