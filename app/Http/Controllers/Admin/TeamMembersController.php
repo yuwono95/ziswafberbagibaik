e26008efa1355b2e5b3faf73cac23414843b3f6e
Yuwono Mujahidin <yuwono@badr-interactive.com>
2021-12-02 01:06:36 +0700

    Update TeamMembersController.php

--- app/Http/Controllers/Admin/TeamMembersController.php
+++ app/Http/Controllers/Admin/TeamMembersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Team;
 use App\Models\User;
+use App\Models\Role;
 use Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
@@ -17,7 +18,7 @@ class TeamMembersController extends Controller
     {
         $team  = Team::where('owner_id', auth()->user()->id)->first();
         $users = User::where('team_id', $team->id)->get();
-		$roles = Role::pluck('name','id');
+        $roles = Role::pluck('name','id');
 
         return view('admin.team-members.index', compact('team', 'users', 'roles'));
     }
