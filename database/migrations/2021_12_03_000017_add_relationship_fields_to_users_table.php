a239da2f575fedb61cc1acabdc865c60dda942e7
Yuwono Mujahidin <yuwono@badr-interactive.com>
2021-12-03 21:48:17 +0700

    Update 2021_12_03_000017_add_relationship_fields_to_users_table.php

--- database/migrations/2021_12_03_000017_add_relationship_fields_to_users_table.php
+++ database/migrations/2021_12_03_000017_add_relationship_fields_to_users_table.php
@@ -10,9 +10,7 @@ class AddRelationshipFieldsToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('kecamatan_id')->nullable();
-            $table->foreign('kecamatan_id', 'kecamatan_fk_5488801')->references('id')->on('kecamatans');
             $table->unsignedBigInteger('team_id')->nullable();
-            $table->foreign('team_id', 'team_fk_5467500')->references('id')->on('teams');
         });
     }
 }
