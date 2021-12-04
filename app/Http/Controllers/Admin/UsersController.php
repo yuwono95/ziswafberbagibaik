c8a5af553a0c7b4b921805e8be8cca22dd508fb0
Yuwono Mujahidin <yuwono@badr-interactive.com>
2021-12-05 01:34:49 +0700

    Update UsersController.php

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -27,7 +27,10 @@ class UsersController extends Controller
         $roleid = \App\Traits\MultiTenantModelTrait::getRoleId();
         if ($request->ajax()) {
             $query = User::with(['kecamatan', 'roles', 'team'])->select(sprintf('%s.*', (new User())->table));
-            if($roleid > 2) {
+			if($roleid == 2) {
+				$query = $query->join('role_user','users.id','=','role_user.user_id')->where('role_user.role_id', '>', 1)
+			}
+            elseif($roleid > 2) {
                 $query = $query->join('role_user','users.id','=','role_user.user_id')->where('role_user.role_id', '>=', $roleid)->where('users.kecamatan_id', '=', auth()->user()->kecamatan_id);
             }
             $table = Datatables::of($query);
