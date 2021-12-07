383bc1b42389edd5c0c0533473c720c76314ba3f
Yuwono Mujahidin <yuwono@badr-interactive.com>
2021-12-07 10:35:01 +0700

    Update TopTenAnggotaController.php

--- app/Http/Controllers/Admin/TopTenAnggotaController.php
+++ app/Http/Controllers/Admin/TopTenAnggotaController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\MassDestroyTopTenAnggotumRequest;
-use App\Http\Requests\StoreTopTenAnggotumRequest;
-use App\Http\Requests\UpdateTopTenAnggotumRequest;
+use App\Http\Requests\MassDestroyTopTenAnggotaRequest;
+use App\Http\Requests\StoreTopTenAnggotaRequest;
+use App\Http\Requests\UpdateTopTenAnggotaRequest;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,58 +14,58 @@ class TopTenAnggotaController extends Controller
 {
     public function index()
     {
-        abort_if(Gate::denies('top_ten_anggotum_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('top_ten_anggota_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return view('admin.topTenAnggota.index');
     }
 
     public function create()
     {
-        abort_if(Gate::denies('top_ten_anggotum_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('top_ten_anggota_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return view('admin.topTenAnggota.create');
     }
 
-    public function store(StoreTopTenAnggotumRequest $request)
+    public function store(StoreTopTenAnggotaRequest $request)
     {
-        $topTenAnggotum = TopTenAnggotum::create($request->all());
+        $topTenAnggota = TopTenAnggota::create($request->all());
 
         return redirect()->route('admin.top-ten-anggota.index');
     }
 
-    public function edit(TopTenAnggotum $topTenAnggotum)
+    public function edit(TopTenAnggota $topTenAnggota)
     {
-        abort_if(Gate::denies('top_ten_anggotum_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('top_ten_anggota_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.topTenAnggota.edit', compact('topTenAnggotum'));
+        return view('admin.topTenAnggota.edit', compact('topTenAnggota'));
     }
 
-    public function update(UpdateTopTenAnggotumRequest $request, TopTenAnggotum $topTenAnggotum)
+    public function update(UpdateTopTenAnggotaRequest $request, TopTenAnggota $topTenAnggota)
     {
-        $topTenAnggotum->update($request->all());
+        $topTenAnggota->update($request->all());
 
         return redirect()->route('admin.top-ten-anggota.index');
     }
 
-    public function show(TopTenAnggotum $topTenAnggotum)
+    public function show(TopTenAnggota $topTenAnggota)
     {
-        abort_if(Gate::denies('top_ten_anggotum_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('top_ten_anggota_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.topTenAnggota.show', compact('topTenAnggotum'));
+        return view('admin.topTenAnggota.show', compact('topTenAnggota'));
     }
 
-    public function destroy(TopTenAnggotum $topTenAnggotum)
+    public function destroy(TopTenAnggota $topTenAnggota)
     {
-        abort_if(Gate::denies('top_ten_anggotum_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('top_ten_anggota_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $topTenAnggotum->delete();
+        $topTenAnggota->delete();
 
         return back();
     }
 
-    public function massDestroy(MassDestroyTopTenAnggotumRequest $request)
+    public function massDestroy(MassDestroyTopTenAnggotaRequest $request)
     {
-        TopTenAnggotum::whereIn('id', request('ids'))->delete();
+        TopTenAnggota::whereIn('id', request('ids'))->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
     }
